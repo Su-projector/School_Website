985b5e3655e4941fdae5daf09be314a556231d60
James <ayomideyolasunkanmi25@gmail.com>
2026-01-13 22:07:41 +0100

    Remove venv from repository and ignore it

--- krelis_legacy_college/krelis_legacy_college/settings.py
+++ krelis_legacy_college/krelis_legacy_college/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +137,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'haryohmideyokikiola@gmail.com'
-EMAIL_HOST_PASSWORD = 'gits txfk frrt hzrh'
-DEFAULT_FROM_EMAIL = 'haryohmideyokikiola@gmail.com'
-SCHOOL_ADMIN_EMAIL = 'effiongephraim95@gmail.com'
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+SCHOOL_ADMIN_EMAIL = os.getenv("SCHOOL_ADMIN_EMAIL")
